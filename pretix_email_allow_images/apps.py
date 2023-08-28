93a05c958279f013290ddf5b38267a5f660786b1
Mira Weller <weller@rami.io>
2023-08-28 17:44:14 +0200

    initial commit - empty plugin

---
+++ pretix_email_allow_images/apps.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+from . import __version__
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(PluginConfig):
+    default = True
+    name = "pretix_email_allow_images"
+    verbose_name = "Allow images in emails"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Allow images in emails")
+        author = "pretix team"
+        description = gettext_lazy(
+            "Allow images in organizer-generated email templates"
+        )
+        visible = True
+        version = __version__
+        category = "CUSTOMIZATION"
+        compatibility = "pretix>=2023.8.0"
+
+    def ready(self):
+        from . import signals  # NOQA
