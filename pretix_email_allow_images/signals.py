d46250f8daa35cbc1977f4bc3234d23d7f0c041d
Mira <weller@rami.io>
2023-08-30 18:40:28 +0200

    implementation (#1)

--- pretix_email_allow_images/signals.py
+++ pretix_email_allow_images/signals.py
@@ -1,1 +1,41 @@
 # Register your receivers here
+from django.dispatch import receiver
+from django.utils.translation import gettext_lazy as _
+from pretix.base.email import TemplateBasedMailRenderer
+from pretix.base.signals import register_html_mail_renderers
+from pretix.base.templatetags.rich_text import (
+    ALLOWED_ATTRIBUTES,
+    ALLOWED_TAGS,
+    markdown_compile_email,
+)
+
+
+class TemplateBasedMailRendererWithImgs(TemplateBasedMailRenderer):
+    def compile_markdown(self, plaintext):
+        return markdown_compile_email(
+            source=plaintext,
+            allowed_tags=ALLOWED_TAGS + ["img"],
+            allowed_attributes=dict(ALLOWED_ATTRIBUTES, img=["src", "alt", "title"]),
+        )
+
+
+class ClassicMailRendererWithImgs(TemplateBasedMailRendererWithImgs):
+    verbose_name = _("Default (images allowed)")
+    identifier = "classic_with_imgs"
+    thumbnail_filename = "pretixbase/email/thumb.png"
+    template_name = "pretixbase/email/plainwrapper.html"
+
+
+class UnembellishedMailRendererWithImgs(TemplateBasedMailRendererWithImgs):
+    verbose_name = _("Simple with logo (images allowed)")
+    identifier = "simple_logo_with_imgs"
+    thumbnail_filename = "pretixbase/email/thumb_simple_logo.png"
+    template_name = "pretixbase/email/simple_logo.html"
+
+
+@receiver(
+    register_html_mail_renderers,
+    dispatch_uid="pretix_email_allow_images_email_renderers",
+)
+def base_renderers_with_imgs(sender, **kwargs):
+    return [ClassicMailRendererWithImgs, UnembellishedMailRendererWithImgs]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,34 @@
-def test_empty():
-    # put your first tests here
-    assert 1 + 1 == 2
+import pytest
+from datetime import timedelta
+from django.utils.timezone import now
+from django_scopes import scopes_disabled
+from pretix.base.models import Event, Organizer
+
+from pretix_email_allow_images.signals import ClassicMailRendererWithImgs
+
+
+@pytest.mark.django_db
+@scopes_disabled()
+def test_markdown_img():
+    organizer = Organizer.objects.create(name="Dummy", slug="dummy")
+    event = Event.objects.create(
+        organizer=organizer,
+        name="Dummy",
+        slug="dummy",
+        date_from=now(),
+        date_admission=now() - timedelta(hours=1),
+        date_to=now() + timedelta(hours=1),
+        testmode=True,
+    )
+    renderer = ClassicMailRendererWithImgs(event)
+
+    source = "![my image](https://example.org/my-image.jpg)"
+    html = renderer.render(
+        plain_body=source,
+        plain_signature="",
+        subject="Hello",
+        order=None,
+        position=None,
+    )
+
+    assert '<img alt="my image" src="https://example.org/my-image.jpg">' in html
